c3806fa1637a98158274f290e762a192bec5a953
Mayank Mehta <mayank4185@csu.fullerton.edu>
2016-06-20 02:55:43 -0700

    Add files via upload
    Arduino Nano code for RC controller

---
+++ RC_nano.ino
@@ -0,0 +1,26 @@
+#define RC_IN 7 
+#define mode 8
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(RC_IN,INPUT);
+pinMode(mode, OUTPUT);
+Serial.begin(115200);
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  //Serial.println(pulseIn(RC_IN,HIGH));
+  if (pulseIn(RC_IN,HIGH)<1000)
+  {
+    //Serial.println(pulseIn(RC_IN,HIGH));
+    digitalWrite(mode,LOW);
+  }
+  else
+  {
+    //Serial.println("1");
+    digitalWrite(mode,HIGH);
+  }
+    
+}
